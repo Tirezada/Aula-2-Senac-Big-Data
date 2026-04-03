63e0130fe8fdfbea64d154cb79bc08a2d19f83a3
Tirezada <christian.espindula@gmail.com>
2026-04-02 21:13:25 -0300

    Atividade Aula 2

---
+++ atividade01.py
@@ -0,0 +1,18 @@
+# numero = int(input('Informe o número: '))
+# anterior = numero - 1
+# sucessor = numero + 1
+
+# print(f'O número digitado foi {numero}')
+# print(f'O seu anterior é {anterior}')
+# print(f'O seu sucessor é {sucessor}')
+
+# -------------------------------------------
+
+# Aumento Salarial
+salario = float(input('Informe o salário: '))
+adicional = salario * 0.15 
+novo_salario = salario + adicional
+
+print(f'O aumento foi de R$ {adicional:.2f}')
+print(f'O novo salário é de R$ {novo_salario:.2f}')
+
---
+++ exemplo01.py
@@ -0,0 +1,49 @@
+# nome = "Maria"
+# idade = 18
+
+# print(f'Olá {nome}')
+# print(f'Você tem {idade} anos')
+#--------------------------------------
+
+# Atividade Fixação (Profissão Bairro)
+
+#-----------------------------------------------
+# Operadores
+preco = 20
+quantidade = 3
+# total = preco * quantidade
+# print(f'O total é {total}')
+
+#--------------------------------------
+
+# Atividade Fixação (Bônus)
+
+#-----------------------------------------------
+
+# Exemplo de Entrada Valores pelo usuário:
+nome = input('Informe o nome: ')
+print(f'Olá {nome}!')
+
+#--------------------------------------
+
+# Atividade Fixação 
+
+#-----------------------------------------------
+salario = float(input('Informe o salário: '))
+bonus = float(input('Informe o bonus: '))
+total_salario = salario + bonus
+print(f'Total: {total_salario}')
+
+# string = texto
+# float = Números decimais
+# int = Número inteiro
+# Boleano = True ou False
+
+# Cálculo de Porcentagem:
+preco = float(input('Informe o Preço: '))
+quantidade = int(input('Informe a quantidade: '))
+total = preco * quantidade
+desconto = total * 0.1 # 10% - Isso é o mesmo de 10 / 100
+valor_final = total - desconto
+print(f'Valor a pagar: {valor_final}')
+print(f'O valor do desconto foi de {desconto}')
