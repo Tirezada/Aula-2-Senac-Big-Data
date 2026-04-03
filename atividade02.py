cad0fff9ad246fad51045a5729a3129bec97971f
Tirezada <christian.espindula@gmail.com>
2026-04-02 21:19:10 -0300

    Add atividade 2

---
+++ atividade02.py
@@ -0,0 +1,15 @@
+numero = int(input("Informe um numero: "))
+
+dobro = numero * 2
+
+triplo = numero * 3
+
+quadrado = numero * numero
+
+print("Seu numero é:", numero)
+
+print("O dobro do seu numero é:", dobro)
+
+print("O triplo do seu numero é:", triplo)
+
+print("O quadrado do seu numero é:", quadrado)
