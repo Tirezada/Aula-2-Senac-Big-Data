fad971de32ef5a20870b285c24f2bb7e9a4ffcb0
Tirezada <christian.espindula@gmail.com>
2026-04-02 21:23:50 -0300

    Add atividade 3

---
+++ atividade03.py
@@ -0,0 +1,9 @@
+nota_01 = float(input("Informe a primeira nota do aluno: "))
+nota_02 = float(input("Informe a segunda nota do aluno: "))
+nota_03 = float(input("Informe a terceira nota do aluno: "))
+nota_04 = float(input("Informe a quarta nota do aluno: "))
+
+media = (nota_01 + nota_02 + nota_03 + nota_04) / 4
+
+print("A media das notas do aluno é:", media)
+
